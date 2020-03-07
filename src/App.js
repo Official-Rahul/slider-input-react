9a7d960eceb8031a21480b848d298a54a1b8fdc0
Official-Rahul <official.rahul.email@gmail.com>
2020-03-07 10:18:12 +0530

    styling implementation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import SliderInputComponent from './components/index'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       value:10,
+       min:0,
+       max:100
+    }
+  }
+  
+
+  changeHandler=(e)=>{
+    this.setState({
+      value:e.target.value
+    })
+  }
+
+  render() {
+    const {min, max, value}=this.state;
+    return (  
+      <div className="App">
+        <SliderInputComponent
+          min={min}
+          max={max}
+          value={value}
+          changeHandler={this.changeHandler}
+        ></SliderInputComponent>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './index.css';
 
 export class SliderInputComponent extends Component {
     render() {
+        const {value, min, max}=this.props;
         return (
-            <input type="range"></input>
+            <input type="range" min={min} max={max} value={value} class="slider" id="myRange" onChange={this.props.changeHandler}/>
         )
     }
 }
